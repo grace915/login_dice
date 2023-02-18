74675669a204465241087cdf982e146b23352a95
grace915 <grace0807@hanyang.ac.kr>
2023-02-18 17:59:23 +0900

    snackbar 하는중

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,61 +34,89 @@ class _LogInState extends State<LogIn> {
             IconButton(icon: Icon(Icons.search), onPressed: () {})
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(padding: EdgeInsets.only(top: 50)),
-              Center(
-                  child: Image(
-                image: AssetImage('dj.gif'),
-                width: 500.0,
-                height: 300.0,
-              )),
-              // form은 입력받을 때 쓰는 위젯
-              Form(
-                  child: Theme(
-                      data: ThemeData(
-                          primaryColor: Colors.teal,
-                          inputDecorationTheme: InputDecorationTheme(
-                              labelStyle: TextStyle(
-                            color: Colors.teal,
-                            fontSize: 15.0,
-                          ))),
-                      child: Container(
-                          padding: EdgeInsets.all(40.0),
-                          child: Column(
-                            children: <Widget>[
-                              TextField(
-                                decoration:
-                                    InputDecoration(labelText: 'Enter "dice"'),
-                                keyboardType: TextInputType.emailAddress,
-                              ),
-                              TextField(
-                                decoration: InputDecoration(
-                                    labelText: 'Enter "password"'),
-                                keyboardType: TextInputType.text,
-                                obscureText: true,
-                              ),
-                              SizedBox(height: 40.0),
-                              ButtonTheme(
-                                  minWidth: 100.0,
-                                  height: 50.0,
-                                  child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.orangeAccent
-                                    ),
-                                      child: Icon(
-                                          Icons.arrow_forward,
-                                      color: Colors.white,
-                                      size: 35.0
-                                      ),
-                                      onPressed: (){}
-                                  )
-                              )
-                            ],
-                          ))))
-            ],
-          ),
+        body: Builder(
+          builder: (context) {
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.only(top: 50)),
+                  Center(
+                      child: Image(
+                    image: AssetImage('dj.gif'),
+                    width: 500.0,
+                    height: 300.0,
+                  )),
+                  // form은 입력받을 때 쓰는 위젯
+                  Form(
+                      child: Theme(
+                          data: ThemeData(
+                              primaryColor: Colors.teal,
+                              inputDecorationTheme: InputDecorationTheme(
+                                  labelStyle: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 15.0,
+                              ))),
+                          child: Container(
+                              padding: EdgeInsets.all(40.0),
+                              child: Column(
+                                children: <Widget>[
+                                  TextField(
+                                    decoration: InputDecoration(
+                                        labelText: 'Enter "dice"'),
+                                    keyboardType: TextInputType.emailAddress,
+                                    controller: controller,
+                                  ),
+                                  TextField(
+                                    decoration: InputDecoration(
+                                        labelText: 'Enter "password"'),
+                                    keyboardType: TextInputType.text,
+                                    obscureText: true,
+                                    controller: controller2,
+                                  ),
+                                  SizedBox(height: 40.0),
+                                  ButtonTheme(
+                                      minWidth: 100.0,
+                                      height: 50.0,
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor:
+                                                Colors.orangeAccent),
+                                        child: Icon(Icons.arrow_forward,
+                                            color: Colors.white, size: 35.0),
+                                        onPressed: () {
+                                          // if 문
+                                          if (controller.text == 'dice' &&
+                                              controller2.text == '1234') {
+                                            //   Navigator.push(context,
+                                            //     MaterialPageRoute(builder: (
+                                            //       BuildContext context) =>
+                                            //     Dice()));
+                                          } else if (controller.text ==
+                                                  'dice' &&
+                                              controller2.text != '1234') {
+                                          } else if (controller.text !=
+                                                  'dice' &&
+                                              controller2.text == '1234') {
+                                          } else {}
+                                        },
+                                      ))
+                                ],
+                              ))))
+                ],
+              ),
+            );
+          },
         ));
   }
+}
+
+void showSnackBar(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '로그인 정보를 다시 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
