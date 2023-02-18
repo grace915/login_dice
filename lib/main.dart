088e959e427d8aaea273b7fb8292946dd90c85b4
grace915 <grace0807@hanyang.ac.kr>
2023-02-18 17:26:59 +0900

    로그인 화면 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,79 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Log in'),
-        backgroundColor: Colors.redAccent,
-        centerTitle: true,
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: (){}
+        appBar: AppBar(
+          title: Text('Log in'),
+          backgroundColor: Colors.redAccent,
+          centerTitle: true,
+          leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
+          ],
         ),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: (){}
-          )
-        ],
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 50)),
+              Center(
+                  child: Image(
+                image: AssetImage('dj.gif'),
+                width: 500.0,
+                height: 300.0,
+              )),
+              // form은 입력받을 때 쓰는 위젯
+              Form(
+                  child: Theme(
+                      data: ThemeData(
+                          primaryColor: Colors.teal,
+                          inputDecorationTheme: InputDecorationTheme(
+                              labelStyle: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 15.0,
+                          ))),
+                      child: Container(
+                          padding: EdgeInsets.all(40.0),
+                          child: Column(
+                            children: <Widget>[
+                              TextField(
+                                decoration:
+                                    InputDecoration(labelText: 'Enter "dice"'),
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              TextField(
+                                decoration: InputDecoration(
+                                    labelText: 'Enter "password"'),
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                              ),
+                              SizedBox(height: 40.0),
+                              ButtonTheme(
+                                  minWidth: 100.0,
+                                  height: 50.0,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.orangeAccent
+                                    ),
+                                      child: Icon(
+                                          Icons.arrow_forward,
+                                      color: Colors.white,
+                                      size: 35.0
+                                      ),
+                                      onPressed: (){}
+                                  )
+                              )
+                            ],
+                          ))))
+            ],
+          ),
+        ));
   }
 }
-
